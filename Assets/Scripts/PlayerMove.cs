020a1ff45bb3b1ecefc1c323449f63d3b3cdfcb5
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-15 15:26:15 +0900

    초기 커밋.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator), typeof(PlayerInput))]
+public class PlayerMove : MonoBehaviour
+{
+	CharacterController characterController;
+	Animator animator;
+
+	public float walkSpeed;
+	public float runSpeed;
+
+	public InputActionAsset controlDefine;
+	InputAction moveAction;
+
+	Vector2 inputValue;
+
+	private void Awake()
+	{
+		characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		controlDefine = GetComponent<PlayerInput>().actions;
+		moveAction = controlDefine.FindAction("Move");
+	}
+
+	private void OnEnable()
+	{
+		moveAction.performed += OnMoveEvent;
+		moveAction.canceled += OnMoveEvent;
+	}
+
+	private void OnDisable()
+	{
+		moveAction.performed -= OnMoveEvent;
+		moveAction.canceled -= OnMoveEvent;
+	}
+
+	public void OnMoveEvent(InputAction.CallbackContext context)
+	{
+		inputValue = context.ReadValue<Vector2>();
+	}
+
+	private void Update()
+	{
+		Vector3 inputMoveDir = new Vector3(inputValue.x, 0, inputValue.y) * walkSpeed;
+		Vector3 actualMoveDir = transform.TransformDirection(inputMoveDir);
+
+		characterController.Move(actualMoveDir * Time.deltaTime);
+
+		animator.SetFloat("Xdir", inputValue.x);
+		animator.SetFloat("Ydir", inputValue.y);
+		animator.SetFloat("Speed", inputValue.magnitude);
+	}
+}
